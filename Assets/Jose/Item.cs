bddae11d3986615281c7a2984847f95e06da6729
Jose <78896687+jv7924@users.noreply.github.com>
2023-11-07 15:29:43 -0800

    Things get added to inventory
    Items can be dropped into an area and they get added to the inventory

--- Assets/Jose/Item.cs
+++ Assets/Jose/Item.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Item", menuName = "Grocery Item")]
-public class Item : ScriptableObject
+// [CreateAssetMenu(fileName = "Item", menuName = "Grocery Item")]
+public class Item : MonoBehaviour
 {
     [SerializeField, TextArea(minLines: 2, maxLines: 6)] private string itemDesc;
     [SerializeField] private string itemName;
     [SerializeField] private int sustScore;
     [SerializeField] private float itemPrice;
-    [SerializeField] private Sprite itemSprite;
+    // [SerializeField] private Sprite itemSprite;
 
     public string GetDescription()
     {
--- Assets/Jose/TestAdd.cs
+++ Assets/Jose/TestAdd.cs
@@ -16,9 +16,32 @@ public class TestAdd : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            inventory.AddItem(newItem);
-        }
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     inventory.AddItem(newItem);
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.R))
+        // {
+        //     inventory.RemoveItem(newItem);
+        // }
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        inventory.AddItem(other.GetComponent<Item>());
+    }
+
+    /// <summary>
+    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerExit(Collider other)
+    {
+        inventory.RemoveItem(other.GetComponent<Item>());
     }
 }
