60e2aa7f4f257400463be242b2e8933bd1637216
Cesar Romero <cromero5503@gmail.com>
2023-11-14 03:44:59 -0800

    Fixed UI for hovering over items

--- Assets/HoverText.cs
+++ Assets/HoverText.cs
@@ -7,61 +7,25 @@ using TMPro;
 
 public class HoverText : MonoBehaviour
 {
-    [SerializeField] string nameStat;
-    [SerializeField] string descriptionStat;
-    [SerializeField] string inOutOfSeasonStat;
-    [SerializeField] string locallySourcedStat;
-    [SerializeField] string fairTradeStat;
+    [SerializeField] GameObject UICanvas;
 
-    private Canvas objectStatsCanvas;
-    private Text objectStatsText;
-    private bool showObjectStats = false;
-
-    private void Start()
-    {
-        // Creates the canvas.
-        objectStatsCanvas = new GameObject("ObjectStatsCanvas").AddComponent<Canvas>();
-        objectStatsCanvas.renderMode = RenderMode.WorldSpace;
-        objectStatsCanvas.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 5;
-        objectStatsCanvas.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-        objectStatsCanvas.enabled = false;
-
-        // ITEM TEXTBOX -----------------------------------------------------------------------------------------------------------
-        GameObject itemName = new GameObject("ItemName");
-        itemName.transform.SetParent(objectStatsCanvas.transform);
-        itemName.transform.LookAt(Camera.main.transform);
-
-        objectStatsText = itemName.AddComponent<Text>();
-        objectStatsText.font = Resources.GetBuiltinResource<Font>("Arial.ttf"); // You can use any font you prefer.
-        objectStatsText.fontSize = 40;
-        objectStatsText.alignment = TextAnchor.UpperCenter;
-        objectStatsText.text = nameStat + "\n" + descriptionStat + "\n" + inOutOfSeasonStat + "\n" + locallySourcedStat + "\n" + fairTradeStat;
-
-        RectTransform textTransformOne = itemName.GetComponent<RectTransform>();
-        textTransformOne.sizeDelta = new Vector2(700, 1000);
-        textTransformOne.anchoredPosition = new Vector2(0, 0);
-    }
+    private bool showCanvas = false;
 
     private void Update()
     {
-        if (showObjectStats)
-        {
-            objectStatsCanvas.enabled = true;
-        }
-        else
-        {
-            objectStatsCanvas.enabled = false;
+        if (showCanvas) {
+            UICanvas.SetActive(true);
+        } else {
+            UICanvas.SetActive(false);
         }
     }
 
-    public void OnHoverEntered(HoverEnterEventArgs args)
-    {
-        showObjectStats = true;
+    public void OnHoverEntered(HoverEnterEventArgs args) {
+        showCanvas = true;
     }
 
-    public void OnHoverExited(HoverExitEventArgs args)
-    {
-        showObjectStats = false;
-        objectStatsCanvas.enabled = false;
+    public void OnHoverExited(HoverExitEventArgs args) {
+        showCanvas = false;
+        UICanvas.SetActive(false);
     }
 }
